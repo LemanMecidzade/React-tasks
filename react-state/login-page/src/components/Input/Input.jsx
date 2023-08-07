4237c74e99b7d9d4da05d5e68a44e44d8def1767
Leman Mecidzade <lmanmcidzad@gmail.com>
2023-08-08 02:20:40 +0400

    f

--- react-state/login-page/src/components/Input/Input.jsx
+++ react-state/login-page/src/components/Input/Input.jsx
@@ -1,4 +1,5 @@
-const Input = ({type,placeholder,onChange}) => {
+const Input = ({type,placeholder,onChange}) => { //{props} //{...props}
+    //const props = {...}
     return (
         <div>
             <input type={type} placeholder={placeholder} onChange={onChange} className="inputs"/>
